073bee49639e910ddcd9474ba9ab615d6bdc1e10
Kaveesha Himasanka <kaveee143@gmail.com>
2022-09-19 09:33:22 +0530

    set login logic

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,9 +1,16 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * @author : Kaveesha Himasanka
@@ -17,10 +24,20 @@ public class LoginFormController {
     public AnchorPane loginContext;
     public TextField txtUsername;
     public PasswordField txtPassword;
+    public Label lblMsg;
 
     public void btnCancelOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnLoginOnAction(ActionEvent actionEvent) {
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        String text = "admin";
+        if (txtUsername.getText().isEmpty() || txtPassword.getText().isEmpty()) {
+            lblMsg.setText("Please enter username and password!");
+        }else if(txtUsername.getText().equalsIgnoreCase(text) & txtPassword.getText().equalsIgnoreCase(text)){
+            Stage window =(Stage) loginContext.getScene().getWindow();
+            window.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../views/InParkingDetailsForm.fxml")))));
+        }else{
+            lblMsg.setText("Invalid username or password!");
+        }
     }
 }
