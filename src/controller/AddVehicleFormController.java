90c1dd79fb31e05439fc9e6026fd4b340a7970e9
Kaveesha Himasanka <kaveee143@gmail.com>
2022-09-19 11:28:47 +0530

    set addVehicle form

---
+++ src/controller/AddVehicleFormController.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * @author : Kaveesha Himasanka
+ * @package : controller
+ * @project : Vehicle parking system
+ * Kaveesha Himasanka
+ * 2022-Sep
+ * @since : 0.1.0
+ **/
+public class AddVehicleFormController {
+}
