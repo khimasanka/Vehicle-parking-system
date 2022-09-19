469368945aa18daf9337b9015d02ee338c552eaf
Kaveesha Himasanka <kaveee143@gmail.com>
2022-09-19 06:35:21 +0530

    set appinitializer

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -6,5 +6,31 @@
  * 2022-Sep
  * @since : 0.1.0
  **/
-public class AppInitializer {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class AppInitializer extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL resource = getClass().getResource("views/HomePageForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Scene scene=new Scene(load);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Vehicle Parking System");
+        primaryStage.show();
+    }
 }
