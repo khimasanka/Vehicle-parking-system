cadd0693ca1595ea65dc4732400635651b77999d
Kaveesha Himasanka <kaveee143@gmail.com>
2022-09-19 05:59:53 +0530

    set packges

---
+++ src/AppInitializer.java
@@ -0,0 +1,10 @@
+/**
+ * @author : Kaveesha Himasanka
+ * @package : PACKAGE_NAME
+ * @project : Vehicle parking system
+ * Kaveesha Himasanka
+ * 2022-Sep
+ * @since : 0.1.0
+ **/
+public class AppInitializer {
+}
